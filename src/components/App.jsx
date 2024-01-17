a9b05a160fae54a9e287ad089a56b028b413742b
Ansh Malik <anshmalik718@gmail.com>
2024-01-17 19:56:01 +0530

    Basic structuring and initialising

---
+++ src/components/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { CssBaseline } from '@mui/material'
+import { Route , Routes } from 'react-router-dom'
+import Actors from './actors/Actors'
+import Navbar from './navbar/Navbar'
+import MovieInformation from './movieInformation/MovieInformation'
+import Movies from './movies/Movies'
+import useStyles from './styles'
+const App = () => {
+    const classes = useStyles()
+  return (
+    <div className={classes.root}>
+        <CssBaseline />
+        <Navbar/>
+            <main className={classes.content}>
+            <div className={classes.toolbar}/>
+            <Routes>
+                    <Route exact path='/' element={<div>Home</div>}>HOME</Route>
+                    <Route exact path='/movie/:id' element={<MovieInformation/>}></Route>
+                    <Route exact path='/actors/:id' element={<Actors/>}></Route>
+                    <Route exact path='/profile/:id' element={<div>Profile</div>}></Route>
+                    {/* <Route path='/'>HOME</Route>
+                    <Route path='/'>HOME</Route> */}
+                </Routes> 
+            </main>
+    </div>
+  )
+}
+
+export default App
